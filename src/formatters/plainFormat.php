dbd5f7fdb04724e86781357a2ac0d4e184ca20aa
eriocl <eriocl@ya.ru>
2020-11-11 19:15:54 +0300

    fixed smell code

--- src/formatters/plainFormat.php
+++ src/formatters/plainFormat.php
@@ -4,33 +4,36 @@ namespace Gendiff\Src\Formatters\Plain;
 
 function makePlain($diffTree, $path = [], $result = '')
 {
-    $formatedTree = array_reduce($diffTree, function ($acc, $node) use ($path) {
-        $status = $node['status'];
-        $key = $node['key'];
-        $path[] = $key;
-        if ($status !== 'nested') {
-            $path = implode('.', $path);
-        }
-        switch ($status) {
-            case 'added':
-                $value = convertValueToPlainString($node['value']);
-                return $acc . "Property '{$path}' was added with value: {$value}\n";
-            case 'deleted':
-                return $acc . "Property '{$path}' was removed\n";
-            case 'unchanged':
-                return $acc;
-            case 'changed':
-                $valueBefore = convertValueToPlainString($node['valueBefore']);
-                $valueAfter = convertValueToPlainString($node['valueAfter']);
-                return $acc . "Property '{$path}' was updated. From {$valueBefore} to {$valueAfter}\n";
-            case 'nested':
-                $children = $node['children'];
-                return makePlain($children, $path, $acc);
-            default:
-                throw new \Exception("Unsupported <{$status}> status in diffTree");
-        }
-    }, $result);
-    return $formatedTree;
+        return array_reduce($diffTree, function ($acc, $node) use ($path) {
+            $status = $node['status'];
+            $key = $node['key'];
+            $path[] = $key;
+            if ($status !== 'nested') {
+                $path = implode('.', $path);
+            }
+            switch ($status) {
+                case 'added':
+                    $value = convertValueToPlainString($node['value']);
+                    $acc .= "Property '{$path}' was added with value: {$value}\n";
+                    break;
+                case 'deleted':
+                    $acc .= "Property '{$path}' was removed\n";
+                    break;
+                case 'unchanged':
+                    return $acc;
+                case 'changed':
+                    $valueBefore = convertValueToPlainString($node['valueBefore']);
+                    $valueAfter = convertValueToPlainString($node['valueAfter']);
+                    $acc .= "Property '{$path}' was updated. From {$valueBefore} to {$valueAfter}\n";
+                    break;
+                case 'nested':
+                    $children = $node['children'];
+                    return makePlain($children, $path, $acc);
+                default:
+                    throw new \Exception("Unsupported <{$status}> status in diffTree");
+            }
+            return $acc;
+        }, $result);
 }
 
 function convertValueToPlainString($value)
